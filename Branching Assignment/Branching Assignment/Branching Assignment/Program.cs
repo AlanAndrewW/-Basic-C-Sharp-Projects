50eb68a28c5ddcdd764a68baf398ca828d55de2d
AlanAndrewW <andrewwood989@hotmail.com>
2025-02-07 11:42:28 -0700

    Add files via upload
    console-based application for getting a shipping quote for a package.
    
    The first line of the program must be: “Welcome to Package Express. Please follow the instructions below.”
    
    The user must then be prompted for the package weight.
    
    If the weight is greater than 50, display the error message, “Package too heavy to be shipped via Package Express. Have a good day.” At this point the program would end.
    
    The user must then be prompted for the package width.
    
    Then the package height.
    
    Then the package length.
    
    If the dimensions total greater than 50, display the error message, “Package too big to be shipped via Package Express.” At this point the program would end.
    
    Next, multiply the three dimensions (height, width, & length) together, and multiply the product by the weight. Finally, divide the outcome by 100.
    
    The result of that calculation is the quote.

---
+++ Branching Assignment/Branching Assignment/Branching Assignment/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Branching_Assignment
+{
+    internal class Program
+    {
+        private static int exitCode;
+
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");//Welcome Printed
+
+
+            Console.WriteLine("Please Enter Your package Weight: ");
+            int weight = Convert.ToInt32(Console.ReadLine());//gather weight info from user input
+            if (weight > 50)//conditional statement. if weight over 50 program will terminate
+            {
+                Console.WriteLine("Package Too Heavy To Be Shipped via Package Express. Have A Good Day.");
+                System.Environment.Exit(exitCode);
+            }
+            else
+            {
+                Console.WriteLine("Thank You.");//thank you will be printed
+            }
+
+            Console.WriteLine("Please Enter Your Package Width: ");
+            int width = Convert.ToInt32(Console.ReadLine());//gather width info from user input
+            if (width > 50)//conditional statement. if width over 50 program will terminate
+            {
+                Console.WriteLine("Package Too Wide To Be Shipped via Package Express. Have A Good Day");
+                System.Environment.Exit(exitCode);
+            }
+            else
+            {
+                Console.WriteLine("Thank You.");
+            }
+
+
+            Console.WriteLine("Please Enter Your Package Height: ");
+            int height = Convert.ToInt32(Console.ReadLine());//conditional statement. if height over 50 program will terminate
+
+            if (height > 50)
+            {
+                Console.WriteLine("Package Too Tall To Be Shipped via Package Express. Have A Good Day");
+                System.Environment.Exit(exitCode);
+            }
+            else
+            {
+                Console.WriteLine("Thank You.");
+            }
+
+
+            Console.WriteLine("Please Enter Your Package Length: ");
+            int length = Convert.ToInt32(Console.ReadLine());//conditional statement. if length over 50 program will terminate
+
+            if (length > 50)
+            {
+                Console.WriteLine("Package Too Long To Be Shipped via Package Express. Have A Good Day");
+                System.Environment.Exit(exitCode);
+            }
+            else
+            {
+                Console.WriteLine("Thank You.");
+            }
+
+            Console.WriteLine("Your Dimensions Are: " + width + " inches x " +  height + " inches x " + length + " Inches.");//print overall dimensions
+            decimal price = width * height * length / 100m;// calculating cost
+            Console.WriteLine("Your Estimated Total will come to: " + price + "$");//printing cost
+            Console.WriteLine("Please Visit Our Nearest Location For More Info Or Call 1-800-478-9999.\n Thank You And Have A Wonderful Day.");
+            System.Environment.Exit(exitCode);//program will terminate on key press
+        }
+    }
+}
