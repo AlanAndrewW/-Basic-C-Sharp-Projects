60049b1413d7f29bf853d19354d056542569ac52
AlanAndrewW <andrewwood989@hotmail.com>
2025-02-10 15:13:53 -0700

    Add files via upload
    simple Console App used for demonstrating string building/concatenation 
    
    Concatenates three strings.
    
    Converts a string to uppercase.
    
    Creates a Stringbuilder and builds a paragraph of text, one sentence at a time.

---
+++ String Assignment/String Assignment/String Assignment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////concatenate 3 strings
+            //string first = "Hello";
+            //string second = " my name is";
+            //string third = " Alan.";
+
+            //Console.WriteLine(String.Concat(first, second, third));
+
+            ////Convert String to uppercase
+            //first = first.ToUpper();
+            //Console.Write(first);
+            //Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Hello  my name is  Alan. It has been a very long day for me.");
+            sb.Append("\nHow are you?\nI myself haven't gotten much sleep and I am Exhausted.\n");
+            sb.Append("I hope your day is going well!");
+            Console.WriteLine(sb);
+            Console.ReadLine();
+           //using stringbuilder to create paragraph of text
+
+           
+        }
+    }
+}
