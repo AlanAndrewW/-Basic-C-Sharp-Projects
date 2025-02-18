55177c9c89d89acfff991d44faf8e0576f22453e
AlanAndrewW <andrewwood989@hotmail.com>
2025-02-17 21:06:04 -0700

    Add files via upload
    Created console app that...
    
    Create a one-dimensional Array of strings. Ask the user to select an index of the Array and then display the string at that index on the screen.
    
    Create a one-dimensional Array of integers. Ask the user to select an index of the Array and then display the integer at that index on the screen.
    
    Add in a message that displays when the user selects an index that doesn’t exist.
    
    Create a list of strings. Ask the user to select an index of the list and then display the content at that index on the screen.

---
+++ ArrayAssignment/ArrayAssingment/ArrayAssingment/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ArrayAssingment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //array of strings
+            string[] stringsPlace = { "United States of America", "Canada", "Mexico", "Japan", "Australia", "Bali", "New Zealand" };
+            //get   user input to display string at said index
+            Console.WriteLine("Please enter a number between 0 and 6 to find out where you should buy a home: ");
+            int stringSelect = Convert.ToInt32(Console.ReadLine());
+            bool stringValid = false;
+
+            while (!stringValid)
+            {
+                try
+                {
+                    Console.WriteLine("Your new home will be... " + stringsPlace[stringSelect]);
+                    stringValid = true;
+                }
+                //message to user if index # invalid
+                catch
+                {
+                    Console.WriteLine("Sorry, That number is not valid. Please select a number between 0 and 6. ");
+                    stringSelect = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+
+            //List of strings
+            List<string> movieList = new List<string>()
+            { "Idiocracy",
+              "Grandma's Boy",
+              "Step Brothers",
+              "Talladega Nights",
+              "Death Note",
+              "One Punch Man",
+              "Attack On Titan"
+            };
+            //get user iput to display string at said index
+            Console.WriteLine("Please Enter a Number Between 0 and 6 To see what Film/Anime Show you could watch tonight: ");
+            int listSelect = Convert.ToInt32(Console.ReadLine());
+            bool listValid = false;
+
+            while (!listValid)
+            {
+                try
+                {
+                    Console.WriteLine("Tonight you should watch... " + movieList[listSelect]);
+                    listValid = true;
+                }
+                //message to user if index # invalid
+                catch
+                {
+                    Console.WriteLine("Sorry, That number is not valid. Please select a number between 0 and 6. ");
+                    listSelect = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+        }
+    }
+}
