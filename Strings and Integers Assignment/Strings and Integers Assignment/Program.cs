fe43c0840b8f057694cac7c4e9c1b3783ad40539
AlanAndrewW <andrewwood989@hotmail.com>
2025-03-13 12:23:38 -0600

    Add files via upload
    Strings and Integers Assignment
    Create a list of integers. Ask the user for a number to divide each number in the list by. Write a loop that takes each integer in the list, divides it by the number the user entered, and displays the result to the screen.
    
    Run the code, entering in non-zero numbers as the user. Look at the displayed results.
    
    Run the code again, entering in zero as the number to divide by. Note any error messages you get.
    
    Run the code once again, entering in a string as the number to divide by. Note any error messages you get.
    
    Now put the loop in a try/catch block. Below and outside of the try/catch block, make the program print a message to the display to let you know the program has emerged from the try/catch block and continued on with program execution. In the catch block, display the error message to the screen. Then try various combinations of user input: valid numbers, zero and a string. Ensure the proper error messages display on the screen, and that the code after the try/catch block gets executed.

---
+++ Strings and Integers Assignment/Strings and Integers Assignment/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings_and_Integers_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create list of ints.
+            List<int> ints = new List<int>() { 100, 40, 1000, 10000, 90, 69, 6000, 4000, 25000, 40000 };
+            //Ask for user input & loop through list dividing each # by user input
+            bool numValid = false;
+            while (!numValid)
+            {
+                //using try/catch for error message
+                try
+                {
+                    Console.WriteLine("\nEnter a number to divide the above list by: ");
+                    float numDivide = float.Parse(Console.ReadLine());
+                    //If statement checks if user enters 0
+                    if (numDivide == 0)
+                    {
+                        Console.WriteLine("Please enter a number different from 0.");
+                    }
+                    //divide each number in list by user input
+                    else
+                    {
+                        foreach (int num in ints)
+                        {
+                            float Divide = num / numDivide;
+                            Console.WriteLine(num + "/" + numDivide + "=" + Divide);
+                        }
+                        numValid = true;
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Input invalid. Please enter digits.");
+                }
+            }
+            Console.WriteLine("The program is terminating now.");
+        }
+    }
+}
